52de0bcda54c19bbe69ee047246602d4c3b71d1f
jinsol kim <jinsol032360@gmail.com>
2025-07-01 17:37:34 +0900

    Expreess의 Request에다가 req.valid <== 옵셔널한 boolean 추가 연습

--- 06-typescript/src/index.ts
+++ 06-typescript/src/index.ts
@@ -7,6 +7,20 @@ const PORT = 3000;
 app.use(express.json());
 app.use("/todos", todoRoutes);
 
+// app.get((req, res, next) => {
+//   req.valid = true;
+//   next();
+// });
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// Expreess의 Request에다가 req.valid <== 옵셔널한 boolean
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       valid?: boolean; // 옵셔널한 boolean
+//     }
+//   }
+// }
